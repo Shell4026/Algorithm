693293a346d91ae77d750d07d443d5122b8fb7ff
Shell <104874910+Shell4026@users.noreply.github.com>
2023-11-17 07:19:57 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	int n;
+	std::cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n - i - 1; ++j)
+			std::cout << ' ';
+		for (int j = 0; j < i + 1; ++j)
+			std::cout << '*';
+		std::cout << '\n';
+	}
+	return 0;
+}
