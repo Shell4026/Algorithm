fbdc020db1bdc5c8b311fd92ac5d2c8d389e3370
Shell <104874910+Shell4026@users.noreply.github.com>
2023-11-17 07:15:29 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+int arr[9] = { 0, };
+void solve(int n, int m, int depth = 0)
+{
+	if (depth == m)
+	{
+		for (int i = 0; i < m; ++i)
+			std::cout << arr[i] << ' ';
+		std::cout << '\n';
+		return;
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		if (depth > 0)
+		{
+			if (i + 1 < arr[depth - 1])
+				continue;
+		}
+		arr[depth] = i + 1;
+		solve( n, m, depth + 1);
+	}
+}
+
+int main()
+{
+	std::cin.tie(0);
+	std::iostream::sync_with_stdio(false);
+
+	int n, m;
+	std::cin >> n >> m;
+
+	solve(n, m);
+	return 0;
+}
