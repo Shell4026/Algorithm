f3f8442d4b93c4a82f90264b7c8936c835c56dd0
Shell <104874910+Shell4026@users.noreply.github.com>
2023-11-19 03:54:12 +0900

    [Silver II] Title: 좌표 압축, Time: 1032 ms, Memory: 38104 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <set>
 #include <algorithm>
 
 int main()
@@ -12,25 +12,25 @@ int main()
 	int n;
 	std::cin >> n;
 
-	std::vector<int> arr(n, 0);
+	std::set<int> set;
 	std::vector<int> original(n, 0);
 	for (int i = 0; i < n; ++i)
-		std::cin >> arr[i];
-
-	original = arr;
-	std::sort(arr.begin(), arr.end());
-	std::map<int, int> comp;
-	
-	for (int i = 0, cnt = 0; i < arr.size(); ++i)
 	{
-		if (comp.find(arr[i]) == comp.end())
-		{
-			comp.insert({arr[i], cnt++});
-		}
+		int num;
+		std::cin >> num;
+		set.insert(num);
+		original[i] = num;
 	}
+	std::vector<int> arr(set.size(), 0);
+	auto it = set.begin();
+	for (int i = 0; i < set.size(); ++i)
+	{
+		arr[i] = *it++;
+	}
+
 	for (auto i : original)
 	{
-		std::cout << comp[i] << ' ';
+		std::cout << std::lower_bound(arr.begin(), arr.end(), i) - arr.begin() << ' ';
 	}
 	return 0;
 }
